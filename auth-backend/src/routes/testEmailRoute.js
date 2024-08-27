35a5fba1f662316b5883826afacee37862644a30
Bhavesh Sidhwani <sidhwanibhavesh@gmail.com>
2024-08-26 23:52:04 -0700

    Added .gitignore

--- auth-backend/src/routes/testEmailRoute.js
+++ auth-backend/src/routes/testEmailRoute.js
@@ -6,7 +6,7 @@ export const testEmailRoute = {
   handler: async (req, res) => {
     try {
       await sendEmail({
-        to: "bhaveshsidhwani007@gmail.com",
+        to: process.env.RECEIVER_EMAIL,
         from: process.env.SENDER_EMAIL,
         subject: "Does this work?",
         text: "If you are reading this... yes!!",
